f71521d50d7013493559067ecd92122d39a6365d
Fernando3743 <fernando201022@hotmail.com>
2021-06-28 19:15:47 -0500

    filterByTaxRate now using Thread-last Operator

--- src/problem1.clj
+++ src/problem1.clj
@@ -4,14 +4,15 @@
 (def c 5)
 (->> c (+ 3) (/ 2) (- 1))
 
-
-
-(defn filterTaxableTaxes [{items :invoice/items}]
-  (filter #(contains? % :taxable/taxes)
-  items))
-
-(defn filterByRate [taxes]
-  (filter #(= (get (nth (get % :taxable/taxes) 0) :tax/rate ) 19) taxes)
+(defn filterByTaxRate [{items :invoice/items}]
+  (->> items (filter #(contains? % :taxable/taxes))
+       (filter #(= (get (nth (get % :taxable/taxes) 0) :tax/rate ) 19)))
   )
 
-(filterByRate (searcher invoice))
+(defn filterRetentions [{items :invoice/items}]
+  (filter #(contains? % :retentionable/retentions)
+          items))
+
+(filterByTaxRate invoice)
+
+(filterByTaxRate invoice)
