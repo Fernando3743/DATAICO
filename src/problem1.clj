31652af944e94ca46da980bb43f1c00db05165c8
Fernando3743 <fernando201022@hotmail.com>
2021-06-29 11:54:49 -0500

    Problem 1 refactored

--- src/problem1.clj
+++ src/problem1.clj
@@ -4,20 +4,25 @@
 (def c 5)
 (->> c (+ 3) (/ 2) (- 1))
 
+(->> invoice (:invoice/items) (first) (:taxable/taxes) (some #(= (:tax/rate %) 19)) (boolean))
+;; END EXAMPLES
+
+
+
+
 (defn filterByTaxRate [{items :invoice/items}]
   (->> items (filter #(contains? % :taxable/taxes))
-       (filter #(= (get (nth (get % :taxable/taxes) 0) :tax/rate) 19)))
-  )
-
+       (filter #(->> % (:taxable/taxes) (some (fn [%] (= (:tax/rate %) 19))) (boolean)))
+  ))
 
 (defn filterByRetentionRate [{items :invoice/items}]
   (->> items (filter #(contains? % :retentionable/retentions))
-       (filter #(= (get (nth (get % :retentionable/retentions) 0) :retention/rate) 1)))
-  )
+       (filter #(->> % (:retentionable/retentions) (some (fn [%] (= (:retention/rate %) 1))) (boolean)))
+       ))
 
 (defn SymmetricDifference  [s1 s2]
   (let [s1 (set s1) s2 (set s2)]
-   (into (clojure.set/difference s1 s2) (clojure.set/difference s2 s1))))
+    (into (clojure.set/difference s1 s2) (clojure.set/difference s2 s1))))
 
 (defn Main [invoice]
   (let [retention (filterByRetentionRate invoice) taxRate (filterByTaxRate invoice)]
@@ -25,4 +30,13 @@
     )
   )
 
+(filterByTaxRate invoice)
+
 (Main invoice)
+
+
+
+
+
+
+
--- src/problem2.clj
+++ src/problem2.clj
@@ -1,18 +1,15 @@
 (in-ns 'invoice-spec)
 
 (defn my-value-reader [key value]
-  (if (re-find #"_date" (str key))
+  (if (re-find #"date" (str key))
     (.parse
       (java.text.SimpleDateFormat. "dd/MM/yyyy")
       value)
     value))
 
-(j/read-str "{\"number\":42,\"date\":\"2012-06-02\"}"
-               :value-fn my-value-reader
-               :key-fn keyword)
-;;=> {:number 42, :date #inst "2012-06-02T04:00:00.000-00:00"}
-
 (def invoice (j/read-str (slurp "src/invoice.json")
                          :key-fn keyword
                          :value-fn my-value-reader))
 (identity invoice)
+
+(s/explain ::invoice invoice)
